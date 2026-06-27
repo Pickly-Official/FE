fcfc12e9cc6f3cd3e2243264209b83e9a4b43024
zio <gio431712@gmail.com>
2026-06-27 06:26:12 +0000

    Add route-based screen structure

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,7 +1,8 @@
-export { default as AuthPage } from './auth/AuthPage';
-export { default as HomePage } from './home/HomePage';
-export { default as CreatePage } from './create/CreatePage';
-export { default as SharePage } from './share/SharePage';
-export { default as VotePage } from './vote/VotePage';
-export { default as ResultPage } from './result/ResultPage';
-export { default as MyPage } from './mypage/MyPage';
+export { default as AuthPage } from './auth';
+export { default as HomePage } from './home';
+export { default as CreatePage } from './create';
+export { default as SharePage } from './share';
+export { default as VotePage } from './vote';
+export { default as ResultPage } from './result';
+export { default as MyPage } from './mypage';
+export { default as LoginPage } from './login';
---
+++ src/pages/login/LoginPage.tsx
@@ -0,0 +1,5 @@
+const LoginPage = () => {
+  return <div>로그인 화면</div>;
+};
+
+export default LoginPage;
